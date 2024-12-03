2d8a1f413a2e85998f5bb2b5cf041c63b5caa4a7
PetarCRO <54545441+PetarCRO@users.noreply.github.com>
2024-12-03 11:56:30 +0100

    dodan ruter

---
+++ src/router.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      component: () => import("@/views/Home.vue"),
+    },
+    // Add more routes here
+    {
+      path: "/treba",
+      component: () => import("@/views/Home.vue"),
+    },
+  ],
+});
+
+export default router;
